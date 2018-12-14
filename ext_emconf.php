971505b954827f536720c0fe4150d31710b7c646
Roland Fuhrer <roland@resterland.ch>
2018-12-14 13:57:50 +0100

    some changes require changes in composer.json

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'stable',
-    'version' => '1.2.1',
+    'version' => '1.2.2',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
