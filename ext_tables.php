b810b34badfb9c4245e9fedf0e9d36c98e6b0c69
Roland Fuhrer <roland@resterland.ch>
2019-07-28 12:52:17 +0200

    felogin localization for Ferman, French and Indonesian language
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_tables.php
+++ ext_tables.php
@@ -1,2 +1,4 @@
 <?php
 defined('TYPO3_MODE') || die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:felogin/Resources/Private/Language/locallang.xlf'][] = 'EXT:resterland/Resources/Private/Language/locallang_felogin.xlf';
