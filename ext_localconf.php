1eaa813b3d850c187bd02d7f8937310c0147fb4e
Roland Fuhrer <roland@resterland.ch>
2019-08-02 12:45:49 +0200

    Still try to get the xlf files of Indonesian language up and running.
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,4 +6,6 @@ defined('TYPO3_MODE') || die();
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['resterland'] = 'EXT:resterland/Configuration/RTE/Default.yaml';
 
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:felogin/locallang.xlf'][] = 'EXT:resterland/Resources/Private/Language/locallangf_felogin.xlf';
+
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['id']['EXT:resterland/Resources/Private/Language/locallang.xlf'][] = 'EXT:resterland/Resources/Private/Language/id.locallangf.xlf';
