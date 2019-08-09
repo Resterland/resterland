0e643b38e07684f5b58b3fbfe6cc54f27471d935
Roland Fuhrer <roland@resterland.ch>
2019-08-09 10:36:16 +0200

    update favicon added some svg files
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '2.1.4',
+    'version' => '2.1.5',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
