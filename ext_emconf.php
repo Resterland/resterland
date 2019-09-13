f9d3b5fa7621898a1e6c8a9e0d0ca5e2e44c8ac7
Roland Fuhrer <roland@resterland.ch>
2019-09-13 10:39:20 +0200

    update composer.lock
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '2.1.6',
+    'version' => '2.2.0',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
