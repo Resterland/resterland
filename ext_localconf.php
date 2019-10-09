bd1af2f812274080158667c127530e187efd33b8
Roland Fuhrer <roland@resterland.ch>
2019-10-09 16:32:44 +0200

    Multilingual copyright text
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,3 +7,10 @@ defined('TYPO3_MODE') || die();
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['resterland'] = 'EXT:resterland/Configuration/RTE/Default.yaml';
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:felogin/locallang.xlf'][] = 'EXT:resterland/Resources/Private/Language/locallang_felogin.xlf';
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['localization']['locales']['user'] = array(
+    'id_ID' => 'Indonesian',
+);
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['localization']['locales']['dependencies'] = array(
+    'id_ID' => array('id'),
+);
