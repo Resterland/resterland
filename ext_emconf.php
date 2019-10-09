fd2a85b7bb87e18b0dad12feea2876047bbe243f
Roland Fuhrer <roland@resterland.ch>
2019-10-09 12:27:53 +0200

    Multilingual copyright text
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '2.3.0',
+    'version' => '2.3.1',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
