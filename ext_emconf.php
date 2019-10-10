27224bfdcd2260b389118e08304641c9cea0772f
Roland Fuhrer <roland@resterland.ch>
2019-10-10 10:50:51 +0200

    some updates for news sitemap
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '2.3.1',
+    'version' => '2.3.3',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
