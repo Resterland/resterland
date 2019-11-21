d7daafe6ad94798b7fb387d1c9fdccece1d80699
Roland Fuhrer <roland@resterland.ch>
2019-11-21 09:46:17 +0100

    Including gallery based on file_list from Xavier Perseguers
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,16 +5,14 @@
  */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Resterland',
-    'description' => 'This extension is based on the bootstrap package extension by Bajamin Kott. Layout switch integrated. Further more pizpalue from Roman Büchler is my godfather extension for many great ideas and samples. Thank you.',
+    'description' => 'This extension is based on the bootstrap package extension by Bajamin Kott. Layout switch integration. Includes a simple gallery based on the extension file_list from Xavier Perseguers.',
     'category' => 'extension',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.9.99',
             'bootstrap_package' => '10.0.0-11.9.99',
             'vhs' => '5.2.0-5.2.99',
-            'gridelements' => '9.3.0-9.3.99',
-            'slickcarousel' => '3.0.2-3.0.99',
-            'ws_flexslider' => '1.5.10-1.5.99',
+            'gridelements' => '9.3.0-9.3.99'
         ],
         'conflicts' => [],
         'suggests' => [],
