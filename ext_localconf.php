744b0bbe90ee07973fd6552ded4bb646a70913c5
Roland Fuhrer <roland@resterland.ch>
2019-12-03 11:09:24 +0100

    Try to setup my own gallery with file_list extension
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_localconf.php
+++ ext_localconf.php
@@ -14,3 +14,16 @@ $GLOBALS['TYPO3_CONF_VARS']['SYS']['localization']['locales']['user'] = array(
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['localization']['locales']['dependencies'] = array(
     'id' => array('id_ID', 'id'),
 );
+
+$boot = function ($_EXTKEY) {
+
+    /* ===========================================================================
+        Register an "image gallery" layout
+    =========================================================================== */
+    $GLOBALS['TYPO3_CONF_VARS']['EXT']['file_list']['templateLayouts'][] = [
+        'Image Gallery', // You may use a LLL:EXT: label reference here of course!
+        'MyGallery',
+    ];
+};
+$boot($_EXTKEY);
+unset($boot);
