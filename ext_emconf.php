d155cde2f09b541c583dbd4bd5772ac4bfcac9bb
Roland Fuhrer <roland@resterland.ch>
2020-09-12 13:37:54 +0200

    update frontend login
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '4.1.0',
+    'version' => '4.1.1',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
