4f6dcb47d14998b533e7dbff3b60383c9cc99eee
Roland Fuhrer <roland@resterland.ch>
2021-04-18 11:29:11 +0200

    adding extension key in composer file
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '4.1.5',
+    'version' => '4.1.7',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
