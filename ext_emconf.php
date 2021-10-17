e8bf1d7add26bc0adf4ecc84c3b86975d4c212b3
Roland Fuhrer <roland@resterland.ch>
2021-10-17 14:33:27 +0200

    update to typo3 11.5
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-10.9.99',
-            'bootstrap_package' => '10.0.0-12.9.99'
+            'bootstrap_package' => '10.0.0-11.9.99'
         ],
         'conflicts' => [],
         'suggests' => [],
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '4.3.0',
+    'version' => '4.2.0',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
