d799391135db6aa9b271c20bd4269fee8364873f
Roland Fuhrer <roland@resterland.ch>
2021-10-24 08:41:11 +0200

    make it Bootstrap5 compatible
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'extension',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.9.99',
+            'typo3' => '10.4.0-11.9.99',
             'bootstrap_package' => '11.0.0-12.9.99'
         ],
         'conflicts' => [],
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '4.2.0',
+    'version' => '5.0.0',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
