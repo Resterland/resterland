89108c0632330f5d533d6dee502e025749d5e9d5
Roland Fuhrer <roland@resterland.ch>
2021-10-25 08:50:25 +0200

    make it Bootstrap5 compatible
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '5.0.0',
+    'version' => '5.1.0',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
