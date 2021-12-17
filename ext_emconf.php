d33a957f87ab8a2ca2e1fa1a02f4b9779b91747b
Roland Fuhrer <roland@resterland.ch>
2021-12-17 11:06:18 +0100

    upgrade to lts 11.5
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '5.0.0',
+    'version' => '5.0.1',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
