d00d3d8df627c3de48954329c6ed24527fffe9e6
Roland Fuhrer <roland@resterland.ch>
2022-03-18 08:36:25 +0100

    composer update
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '5.1.2',
+    'version' => '5.2.0',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
