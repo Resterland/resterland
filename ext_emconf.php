a2d06f4177ba548e46599dcd900b0b2b0e1426ff
Roland Fuhrer <roland@resterland.ch>
2022-04-05 10:30:18 +0200

    clean up code
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = [
         ],
     ],
     'state' => 'beta',
-    'version' => '5.2.1',
+    'version' => '5.2.2',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
