4e610143f79635e6bc2f4d2eea3296ccc79c6a7b
Roland Fuhrer <roland@resterland.ch>
2023-05-30 07:40:09 +0200

    fix some bugs
    Signed-off-by: Roland Fuhrer <roland@resterland.ch>

--- ext_tables.php
+++ ext_tables.php
@@ -3,5 +3,3 @@
 if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
-
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locallangXMLOverride']['EXT:felogin/Resources/Private/Language/locallang.xlf'][] = 'EXT:resterland/Resources/Private/Language/locallang_felogin.xlf';
